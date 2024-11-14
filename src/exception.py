63c592511d568dd6f51f7c77d0cf789c66c66ee0
MuslimAqeel311 <muslimarain321@gmail.com>
2024-11-14 13:50:34 +0500

    EDA and Problems Statement

--- src/exception.py
+++ src/exception.py
@@ -19,8 +19,3 @@ class CustomException(Exception):
         return self.error_message
     
     
-if __name__=="__main__":
-    try:
-        a=1/10
-    except Exception as e:
-        raise CustomException(e, sys)
